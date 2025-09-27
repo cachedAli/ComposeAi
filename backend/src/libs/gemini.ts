5a3823df05cedf812a0ef270b3800b6a7374f465
Mohammad ALI <100780537+cachedAli@users.noreply.github.com>
2025-09-28 02:10:09 +0500

    Update gemini.ts

--- backend/src/libs/gemini.ts
+++ backend/src/libs/gemini.ts
@@ -7,5 +7,5 @@ dotenv.config();
 const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
 
 export const getGeminiModel = () => {
-    return genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
+    return genAi.getGenerativeModel({ model: "gemini-2.0-flash" });
 };
